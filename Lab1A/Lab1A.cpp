e433763b9518f212ae23dd0e2b256ab1ff4bd60d
LeAnda Milliman <lmmilliman4@gmail.com>
2021-08-29 14:40:56 -0400

    Add project files.

---
+++ Lab1A/Lab1A.cpp
@@ -0,0 +1,14 @@
+// Lab1A
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "three...";
+	cout << "two...";
+	cout << "one...";
+	cout << "zero...";
+	cout << "liftoff!" << endl; 
+	// appears on first line 
+	cout << "houston, we have a problem." << endl << endl;
+	return 0;
+}
